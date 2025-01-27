84d98a21b6fc2e6a329503f16c44073417cb0b18
korelov47 <vygld@yandex.com>
2025-01-27 21:45:32 +0300

    feat: Добавляет решение для задачи "Компоненты. Компонент-счётчик"

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -1,6 +1,6 @@
-import { defineComponent } from 'vue'
-import { UiButton } from '@shgk/vue-course-ui'
-import './UiCounter.css'
+import { computed, defineComponent } from 'vue';
+import { UiButton } from '@shgk/vue-course-ui';
+import './UiCounter.css';
 
 export default defineComponent({
   name: 'UiCounter',
@@ -9,15 +9,62 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
-    // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
+  props: {
+    count: {
+      type: Number,
+      required: true,
+    },
+
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    },
   },
 
-  template: `
+  emits: ['update:count'],
+
+  setup(props, { emit }) {
+    const isIncreaseButtonDisabled = computed(() => props.count >= props.max);
+    const isDecreaseButtonDisabled = computed(() => props.count <= props.min);
+
+    const decreaseCount = () => {
+      emit('update:count', props.count - 1);
+    };
+
+    const increaseCount = () => {
+      emit('update:count', props.count + 1);
+    };
+
+    return {
+      isIncreaseButtonDisabled,
+      isDecreaseButtonDisabled,
+      decreaseCount,
+      increaseCount,
+    };
+  },
+
+  template: /* html */ `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton
+        aria-label="Decrement"
+        :disabled="isDecreaseButtonDisabled"
+        @click="decreaseCount"
+      >
+        ➖
+      </UiButton>
+      <span class="count" data-testid="count">{{ count }}</span>
+      <UiButton
+        aria-label="Increment"
+        :disabled="isIncreaseButtonDisabled"
+        @click="increaseCount"
+      >
+        ➕
+      </UiButton>
     </div>
   `,
-})
+});
